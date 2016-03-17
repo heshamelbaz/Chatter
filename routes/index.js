e38fe713048f5bdf0a64eb03d9e5ac7c4371c0f1
Hesham ElBaz <heshamelbaz@hotmail.com>
2016-03-17 09:49:40 +0000

    Customizing Landing Page UI

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('layout', { 
-		page: 'index', title: 'YACA', isLoggedIn: req.isAuthenticated()
- 	});
+	if(!req.isAuthenticated()){
+	  	res.render('layout', { 
+			page: 'index', title: 'YACA', isLoggedIn: req.isAuthenticated(), message: req.flash('signupMessage') 
+  		});
+	} else {
+		res.render('layout', {
+			page: 'users/profile', title: 'YACA', isLoggedIn: req.isAuthenticated(), user: req.user
+		});	
+	}
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,13 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
-router.get('/signup', function(req, res){
-	res.render('layout', { page: 'users/signup.ejs', title: 'YACA', isLoggedIn: req.isAuthenticated(), message: req.flash('signupMessage') });
-});
-
 router.post('/signup', passport.authenticate('local-signup', {
 	successRedirect: '/users/profile',
-	failureRedirect: '/users/signup'
+	failureRedirect: '/'
 }));
 
 router.get('/login', function(req, res){
-	res.render('layout', { page:'users/login.ejs', title: 'YACA', isLoggedIn: req.isAuthenticated(), message: req.flash('loginMessage') } );
+	res.render('layout', { page:'users/login', title: 'YACA', isLoggedIn: req.isAuthenticated(), message: req.flash('loginMessage') } );
 });
 
 router.post('/login', passport.authenticate('local-login', {
@@ -22,7 +18,7 @@ router.post('/login', passport.authenticate('local-login', {
 }));
 
 router.get('/profile', isLoggedIn, function(req, res){
-	res.render('layout', { page: 'users/profile.ejs', title: 'YACA', isLoggedIn: req.isAuthenticated(), user: req.user} );
+	res.render('layout', { page: 'users/profile', title: 'YACA', isLoggedIn: req.isAuthenticated(), user: req.user} );
 });
 
 router.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
