583ccd6ab961a2df34772d529e30e20cd4222f91
Hesham ElBaz <heshamelbaz@hotmail.com>
2016-03-19 08:33:44 +0200

    Fixing DB Typo

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
 	url: {
 		development: 'mongodb://localhost/yaca',
-		production: 'mognodb://yaca:yaca@ds029117.mlab.com:29117/yaca'
+		production: 'mongodb://yaca:yaca@ds029117.mlab.com:29117/yaca'
 	}
 }
